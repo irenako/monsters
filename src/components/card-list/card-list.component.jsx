66b3f1527afcc246c6f555bced3ebc4c3d77cee8
irenako <108225581+irenako@users.noreply.github.com>
2022-11-04 14:18:40 +0200

    Video 58-61

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,12 @@
-import { Component } from "react";
-import "./card-list.styles.css";
 import Card from "../card/card.component";
+import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return (
-            <div>
-              <Card monster={monster} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
